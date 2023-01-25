bccd82eb7a32dec254f82fa2733090efffc4d042
SergeiKozhin <kozhinmail@gmail.com>
2023-01-25 23:28:33 +0400

    25.01.23_5

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,15 +1,25 @@
 
-#1. Методами строк очистить текст от знаков препинания;
+# 1. Методами строк очистить текст от знаков препинания;
 
 import string
 with open('text.txt', encoding='UTF-8') as f:
     file = f.read()
 f.close()
 
-punc = string.punctuation
+punc = string.punctuation + '—«»'
 
+a = file.translate(str.maketrans('', '', punc))
+#print(a)
 
-for i in file:
-    if i in punc:
-        row = file.replace(i, '')
-print(row)
+# 2. сформировать list со словами (split);
+
+b = a.split()
+#print(b)
+
+# 3. привести все слова к нижнему регистру (map);
+
+c = list(map(str.lower, b))
+#print(c)
+
+# 4. получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
+
