0596141bc74758b2ca65abc9d626b1688a63c617
SergeiKozhin <kozhinmail@gmail.com>
2023-01-25 21:57:40 +0400

    25.01.23_3

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,5 +1,15 @@
 
 #1. Методами строк очистить текст от знаков препинания;
 
+import string
 with open('text.txt', encoding='UTF-8') as f:
-    print(f.read())
+    file = f.read()
+f.close()
+
+punc = string.punctuation
+
+
+for i in file:
+    if i in punc:
+        row = file.replace(i, '')
+print(row)
