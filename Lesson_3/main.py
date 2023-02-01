b80ce190a7af446cd7e65a6122813de10b3d8ea3
SergeiKozhin <kozhinmail@gmail.com>
2023-02-01 10:27:06 +0400

    01.02_1

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -23,3 +23,17 @@ c = list(map(str.lower, b))
 
 # 4. получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
 
+from collections import Counter
+
+d = dict(Counter(c))
+#print(d)
+
+#5. вывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).
+
+sorted_dict = {}
+sorted_keys = sorted(d, key=d.get)  # [1, 3, 2]
+
+for w in sorted_keys:
+    sorted_dict[w] = d[w]
+
+print(tuple(sorted_dict))
