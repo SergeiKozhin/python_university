4a85ae33aa8a3f175598869c702df3fd345b5a77
SergeiKozhin <kozhinmail@gmail.com>
2023-02-13 15:33:53 +0400

    13.02_1

--- Lesson_5/divisor_master.py
+++ Lesson_5/divisor_master.py
@@ -1,5 +1,6 @@
-a = 5
+a = 12
 
+# 1. проверка числа на простоту (простые числа - это те числа у которых делители единица и они сами);
 
 def is_prime(a):
     if a < 2:
@@ -9,3 +10,47 @@ def is_prime(a):
             return False
     else:
         return True
+
+
+# 2. выводит список всех делителей числа;
+
+def divisors(a):
+    return [i for i in range(1, a + 1) if a % i == 0 ]
+
+
+# 3. выводит самый большой простой делитель числа
+
+def max_prime_divisor(a):
+    c = 0
+    for x in divisors(a):
+        for i in range(2, int(a ** 0.5 + 1)):
+            if x % i == 0:
+                continue
+            else:
+                c = x
+    return c
+
+
+# 4. функция выводит каноническое разложение числа на простые множители
+
+def kanon(a):
+    i = 2
+    kanon_list = []
+    while i * i <= a:
+        while a % i == 0:
+            kanon_list.append(i)
+            a = a / i
+        i = i + 1
+    if a > 1:
+        kanon_list.append(int(a))
+    return kanon_list
+
+print(kanon(a))
+
+
+# 5. функция выводит самый большой делитель (не обязательно простой) числа.
+def max_divisor(a):
+    return divisors(a)[-1]
+
+
+
