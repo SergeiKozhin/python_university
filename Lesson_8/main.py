33941ecf38474db3f8f11d0cd837b593e6717a59
SergeiKozhin <kozhinmail@gmail.com>
2023-04-23 22:28:11 +0400

    Update main.py
    12

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -1,84 +1,66 @@
-#1. Генератор последовательностей
-def my_sequence_generator(n):
-    i = 0
-    while i < n:
-        yield i
-        i += 1
+# Генератор последовательностей
+def my_generator(start, stop):
+    while start < stop:
+        yield start
+        start += 1
 
+# Тернарный оператор
+def my_ternary(condition, true_value, false_value):
+    return true_value if condition else false_value
 
-Тернарный оператор
-
-def my_ternary_operator(a, b):
-    return a if a > b else b
-
-
-#2. Декоратор
 def my_decorator(func):
     def wrapper(*args, **kwargs):
-        print("Decorator is working")
+        print("Before function call")
         result = func(*args, **kwargs)
+        print("After function call")
         return result
     return wrapper
 
+@my_decorator
+def my_function():
+    print("Function called")
 
-#3. Декоратор, замеряющий время выполнения
-from time import time
+my_function()
 
+import time
 
-def time_execution(func):
+def timeit(func):
     def wrapper(*args, **kwargs):
-        start = time()
+        start = time.time()
         result = func(*args, **kwargs)
-        end = time()
-        print("Execution time: {}".format(end - start))
+        end = time.time()
+        print("Time taken:", end - start, "seconds.")
         return result
     return wrapper
 
+@timeit
+def my_function():
+    time.sleep(2)
+    print("Function completed")
 
-#4. Сравнение времени создания генератора и списка
-def generate_sequence_using_list(n):
+my_function()
+
+import time
+
+def create_list(stop):
+    start = 1
     my_list = []
-    for i in range(n):
-        my_list.append(i)
+    while start <= stop:
+        my_list.append(start)       
+        start += 1
     return my_list
 
+def create_generator(stop):
+    start = 1
+    while start <= stop:
+        yield start
+        start += 1
+        start = time.time()
+my_list = create_list(1000000)
+end = time.time()
+print("Time taken to create list:", end - start, "seconds.")
 
-@time_execution
-def time_list_creation(n):
-    my_list = generate_sequence_using_list(n)
-
-
-@time_execution
-def time_generator_creation(n):
-    my_generator = my_sequence_generator(n)
-    for i in my_generator:
-        pass
-
-
-#5. Декоратор, замеряющий объем оперативной памяти
-import psutil
-
-
-def memory_usage(func):
-    def wrapper(*args, **kwargs):
-        process = psutil.Process()
-        memory_before = process.memory_info().rss / 1024 / 1024
-        result = func(*args, **kwargs)
-        memory_after = process.memory_info().rss / 1024 / 1024
-        print("Memory usage: {} MB".format(memory_after - memory_before))
-        return result
-    return wrapper
-
-
-#6. Сравнение объема оперативной памяти для функции создания генератора и списка
-@memory_usage
-def memory_list_creation(n):
-    my_list = generate_sequence_using_list(n)
-
-
-@memory_usage
-def memory_generator_creation(n):
-    my_generator = my_sequence_generator(n)
-    for i in my_generator:
-        pass
-
+start = time.time()
+my_generator = create_generator(1000000)
+end = time.time()
+print("Time taken to create generator:", end - start, "seconds.")
